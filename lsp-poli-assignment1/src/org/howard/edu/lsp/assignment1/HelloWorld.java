24de1d773a93481c8a981df87c8f299892592b9a
prem2 <prem2@10.0.0.153>
2022-09-01 13:25:31 -0400

    This is my first commit for assignment1.

---
+++ lsp-poli-assignment1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment1;
+
+/*
+ * Prem Oli
+ * @author devpremoli
+ * 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
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
